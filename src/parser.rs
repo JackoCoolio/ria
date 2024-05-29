ef5a6d4799ab4e903f3461dd7ee95e0b8e94d962
Jackson Wambolt <jtwambolt@gmail.com>
2024-05-29 06:06:14 +0000

    style: Remove duplicate dead code
    Not sure how I missed this.

--- src/parser.rs
+++ src/parser.rs
@@ -1,25 +1,13 @@
 use winnow::{
-    combinator::{alt, cut_err},
-    error::{AddContext, ContextError, ErrMode, InputError},
-    stream::{Location, Stream},
+    error::{ContextError, ErrMode},
+    stream::Stream,
     PResult, Parser,
 };
 
-use crate::lexer::{Lexer, Spanned, Symbol, Token};
-
-use self::expr::Expr;
+use crate::lexer::{Spanned, Symbol, Token};
 
 mod def;
 mod expr;
-
-struct Module<'i> {
-    defs: Box<[Def<'i>]>,
-}
-
-struct Def<'i> {
-    ident: Spanned<&'i str>,
-    expr: Expr<'i>,
-}
 
 fn ident<'i, S>(input: &mut S) -> PResult<Spanned<&'i str>>
 where
@@ -54,29 +42,4 @@ where
         }
         _ => Err(ErrMode::Backtrack(ContextError::new())),
     }
-}
-
-impl<'i> Def<'i> {
-    pub fn parse<S>(input: &mut S) -> PResult<Def<'i>>
-    where
-        S: Stream<Token = Spanned<Token<'i>>>,
-    {
-        let ident = ident.parse_next(input)?;
-        let _ = symbol(&Symbol::Define).parse_next(input)?;
-        let expr = Expr::parse.parse_next(input)?;
-
-        Ok(Self { ident, expr })
-    }
-}
-
-#[test]
-fn parse_simple_def() {
-    let input = "x = y";
-    let lexer = Lexer::new(input);
-    let tokens: Box<[Spanned<Token>]> = lexer.collect();
-
-    let def = Def::parse.parse(tokens.as_ref()).unwrap();
-
-    assert_eq!(def.ident.inner(), "x");
-    assert!(matches!(def.expr, Expr::Variable(Spanned("y", _))));
 }
