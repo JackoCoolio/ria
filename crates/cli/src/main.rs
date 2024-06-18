7051fce05b000595c7f1a2f84e4049cd708cf9d3
Jackson Wambolt <jtwambolt@gmail.com>
2024-06-18 05:44:27 +0000

    feat(cli): Create `ria_cli` package for CLI tool
    This will be the entry point to the compiler. For now, it just parses an
    expression and debug prints it.

---
+++ crates/cli/src/main.rs
@@ -0,0 +1,24 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+use ria_lexer::Lexer;
+use ria_parser::expr::Expr;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The source filepath
+    #[arg(name = "file")]
+    source_file: PathBuf,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let source = std::fs::read_to_string(args.source_file).unwrap();
+
+    let tokens = Lexer::new(&source).collect::<Box<_>>();
+    let ast = Expr::parse(&mut tokens.as_ref()).unwrap();
+
+    println!("parsed:\n{:?}", ast);
+}
